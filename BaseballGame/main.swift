e21befb77a1cf5bd53587e416651aa7bf379840f
kseunghui@gmail.com <kseunghui@gmail.com>
2024-06-13 20:21:22 +0900

    [Add] 과제 1-6레벨 뼈대 구현

--- BaseballGame/main.swift
+++ BaseballGame/main.swift
@@ -4,8 +4,32 @@
 //
 //  Created by 김승희 on 6/13/24.
 //
-
 import Foundation
 
-print("Hello, World!")
+var nums = (0...9).map{$0}
+var logs = [Int: Int]()
+var round = 0
+
+while true {
+    print("환영합니다! 원하시는 번호를 입력해주세요.\n1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기")
+    let gameNum = readLine()!
+    var count = 0
+    
+    switch Int(gameNum) {
+    case 1:
+        print()
+    case 2:
+        print("< 게임 기록 보기 >")
+        for (key, val) in logs {
+            print("\(key)번째 게임 : 시도 횟수 - \(val)")
+        }
+    case 3:
+        print("< 숫자 야구 게임을 종료합니다 >")
+        round = 0
+        logs = [:]
+        break
+    default:
+        print("올바른 숫자를 입력해주세요!")
+    }
+}
 
